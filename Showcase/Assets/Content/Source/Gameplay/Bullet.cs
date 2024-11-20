776b575d0b6b1adfb11e9bd2dce60a014eefd926
Cat <cat.ursu30@gmail.com>
2024-11-20 16:00:01 +0300

    Added prefab for bullet and logic for it

---
+++ Showcase/Assets/Content/Source/Gameplay/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using IdleNumbers;
+using UnityEngine;
+using Helpers.PoolSystem;
+
+namespace Source.Gameplay
+{
+public class Bullet : PooledObject
+{
+    [SerializeField] float _speed;
+    [SerializeField] Rigidbody2D _rb;
+    
+    IdleNumber _hitDamage;
+
+    public override void Init(Action<PooledObject> onReleaseToPool, object config)
+    {
+        base.Init(onReleaseToPool, config);
+
+        if (config is IdleNumber hitDamage) 
+            _hitDamage = hitDamage;
+    }
+
+    public override void Set(object config = null)
+    {
+        _rb.linearVelocity = Vector2.up * _speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.TryGetComponent(out EnemyHitTrigger enemyCollider))
+        {
+            Vector2 impulseDirection = other.transform.position - transform.position;
+            enemyCollider.OnHit_raise(_hitDamage, impulseDirection);
+        }
+
+        OnReleaseToPool_raise();
+    }
+}
+}
