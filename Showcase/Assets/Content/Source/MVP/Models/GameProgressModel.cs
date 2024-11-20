4efcd7094321de060921a14473912dcfef7c03a6
Cat <cat.ursu30@gmail.com>
2024-11-20 16:07:59 +0300

    Added model and presenter for game progress

---
+++ Showcase/Assets/Content/Source/MVP/Models/GameProgressModel.cs
@@ -0,0 +1,41 @@
+﻿using UniRx;
+using System.Linq;
+using IdleNumbers;
+using Source.Session;
+
+namespace Source.MVP
+{
+public class GameProgressModel
+{
+    public ReactiveProperty<float> Progress { get; private set; }
+    public ReactiveProperty<IdleNumber> CollectedCoins { get; private set; }
+    
+    public bool PlayerIsAlive;
+    public IdleNumber RewardCoins;
+
+    readonly bool _vibrationOn;
+    readonly float _shipCoinBonus;
+    SessionProgress _sessionProgress;
+
+    public GameProgressModel()
+    {
+        var sessionManager = SessionManager.Current;
+        _sessionProgress = sessionManager.Progress;
+        _shipCoinBonus = sessionManager.Items.Ships.ElementAt(_sessionProgress.UsedShipIdx.Value).Value.EnemyCoinBonus.Value / 100 + 1;
+
+        Progress = new(0);
+        CollectedCoins = new(new IdleNumber());
+    }
+
+    public void AddKillReward(float rewardPoints, IdleNumber coins)
+    {
+        if (!PlayerIsAlive) return;
+
+        RewardCoins = (_sessionProgress.Lvl.Value * ConstGameplay.COINS_LVL_BONUS_MULTIPLIER * coins + coins) * _shipCoinBonus;
+
+        _sessionProgress.Coins.Value += RewardCoins;
+        CollectedCoins.Value += RewardCoins;
+        Progress.Value += rewardPoints;
+    }
+}
+}
---
+++ Showcase/Assets/Content/Source/MVP/Presenters/GameProgressPresenter.cs
@@ -0,0 +1,82 @@
+﻿using UniRx;
+using IdleNumbers;
+using Source.Session;
+
+namespace Source.MVP
+{
+public class GameProgressPresenter
+{
+#region Fields
+
+    bool _startNewSession;
+    GameProgressModel _gameProgressModel;
+    CompositeDisposable _disposable;
+    IViewGameplay _viewGameplay;
+
+#endregion
+
+#region Public methods
+
+    public GameProgressPresenter(IViewGameplay viewGameplay, GameProgressModel model)
+    {
+        _viewGameplay = viewGameplay;
+        _gameProgressModel = model;
+    }
+
+    public void Init()
+    {
+        _disposable = new CompositeDisposable();
+        _gameProgressModel.Progress.Value = 0;
+        _gameProgressModel.CollectedCoins.Value *= 0;
+        _gameProgressModel.PlayerIsAlive = true;
+
+        _gameProgressModel.Progress.Skip(1).Subscribe(OnProgressChange_handler).AddTo(_disposable);
+        _gameProgressModel.CollectedCoins.Skip(1).Subscribe(OnChangeCoinsAmount_handler).AddTo(_disposable);
+        SessionManager.Current.Progress.Lvl.Skip(1).Subscribe(OnReachNewLvl_handler).AddTo(_disposable);
+
+        _viewGameplay.SetNewGameInfo(SessionManager.Current.Progress.Lvl.Value);
+    }
+
+    public void Deinit()
+    {
+        _disposable.Dispose();
+        _gameProgressModel.PlayerIsAlive = false;
+    }
+
+#endregion
+
+#region Private methods
+
+    void OnChangeCoinsAmount_handler(IdleNumber coins)
+    {
+        _viewGameplay.OnChangeCoins_handler(coins);
+    }
+
+    void OnProgressChange_handler(float progress)
+    {
+        if (progress < ConstGameplay.PROGRESS_TARGET)
+        {
+            if (!_startNewSession)
+                _viewGameplay.OnChangeProgress_handler(progress);
+        }
+        else
+            SessionManager.Current.Progress.Lvl.Value++;
+    }
+
+    void OnReachNewLvl_handler(int lvl)
+    {
+        SessionManager.Current.Save(ESaveFileType.Progress);
+        _startNewSession = true;
+        _gameProgressModel.Progress.Value = 0;
+        _viewGameplay.PlayNewLvlAnimation(lvl, FinishNewLvlAnimation);
+    }
+    
+    void FinishNewLvlAnimation()
+    {
+        _startNewSession = false;
+        OnProgressChange_handler(_gameProgressModel.Progress.Value);
+    }
+
+#endregion
+}
+}
