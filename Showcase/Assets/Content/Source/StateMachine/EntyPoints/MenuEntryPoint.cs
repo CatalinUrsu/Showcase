3d63bab870dda71b946076a18c85aad17b274d7a
Cat <cat.ursu30@gmail.com>
2024-11-20 14:04:58 +0300

    Added EntryPoint and Mediator for menu

---
+++ Showcase/Assets/Content/Source/StateMachine/EntyPoints/MenuEntryPoint.cs
@@ -0,0 +1,52 @@
+using System;
+using Helpers;
+using UnityEngine;
+using StateMachine;
+using Cysharp.Threading.Tasks;
+
+namespace Source.StateMachine
+{
+public class MenuEntryPoint : MonoBehaviour, IEntryPoint
+{
+#region Fields
+
+    [SerializeField] BankLoader _bankLoader;
+    [SerializeField] MenuMediator _menuMediator;
+
+    StatesMachine _statesMachine;
+
+#endregion
+
+#region Public methods
+    
+    public async UniTask Init(StatesMachine statesMachine, Action<float> onUpdateProgress)
+    {
+        _statesMachine = statesMachine;
+        
+        await _bankLoader.Init();
+        await _menuMediator.Init(onUpdateProgress);
+        _menuMediator.OnClickStartGame += () => StartGame().Forget();
+    }
+
+    public async UniTask Enter() => await _menuMediator.ShowPlayer();
+
+    /// <summary>
+    /// Deinit content on scene. In this project there's no cases when need to wait something, but it may be in other projects (ex: save data, network operation).
+    /// </summary>
+    public async UniTask Exit()
+    {
+        _menuMediator.Deinit();
+        _bankLoader.Deinit();
+
+        await UniTask.CompletedTask;
+    }
+
+#endregion
+    
+    async UniTaskVoid StartGame()
+    {
+        using (InputManager.Instance.LockInputSystem())
+            await _statesMachine.Enter<StateGameplay>();
+    }
+}
+}
---
+++ Showcase/Assets/Content/Source/StateMachine/Mediators/MenuMediator.cs
@@ -0,0 +1,76 @@
+﻿using TMPro;
+using System;
+using Source.UI;
+using UnityEngine;
+using DG.Tweening;
+using Source.Player;
+using UnityEngine.UI;
+using Cysharp.Threading.Tasks;
+using Helpers;
+
+namespace Source.StateMachine
+{
+public class MenuMediator : MonoBehaviour
+{
+#region Fields
+
+    [SerializeField] PlayerFacadeMenu _playerFacadeMenu;
+    [SerializeField] TabsGroup _tabsGroup;
+
+    [Header("Start Button")] 
+    [SerializeField] Button _startBtn;
+    [SerializeField] TextMeshProUGUI _startBtnText;
+
+    public event Action OnClickStartGame;
+
+    Sequence _startbuttonSequence;
+
+#endregion
+
+#region Public methods
+
+    public async UniTask Init(Action<float> onUpdateProgress)
+    {
+        _playerFacadeMenu.Init();
+        SetStartButtonAnim();
+
+        await _tabsGroup.InitContent(onUpdateProgress);
+    }
+
+    public void Deinit()
+    {
+        _playerFacadeMenu.Deinit();
+        _startbuttonSequence.CheckAndEnd(false);
+    }
+
+    public async UniTask ShowPlayer() => await _playerFacadeMenu.ShowPayer();
+
+#endregion
+
+#region Private methods
+
+    void SetStartButtonAnim()
+    {
+        _startBtn.onClick.AddListener(OnClickStart_handler);
+
+        _startbuttonSequence = DOTween.Sequence()
+                                      .Append(_startBtnText.rectTransform.DOScale(1.25f, .5f).From(1f))
+                                      .Join(_startBtnText.DOFade(.1f, .5f).From(.5f))
+                                      .SetLoops(-1, LoopType.Yoyo);
+    }
+
+    void OnClickStart_handler()
+    {
+        OnClickStartGame?.Invoke();
+
+        _startbuttonSequence.CheckAndEnd(false);
+        _startbuttonSequence = DOTween.Sequence()
+                                      .Append(_startBtnText.rectTransform.DOMoveX(Screen.width, .5f)
+                                                           .SetRelative()
+                                                           .SetEase(Ease.InBack))
+                                      .Join(_startBtnText.DOFade(.5f, 0));
+    }
+
+#endregion
+}
+}
