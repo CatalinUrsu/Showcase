8eb24ccfe9a7d29da78f6d155b9e1344f9dbb163
Cat <cat.ursu30@gmail.com>
2024-11-20 14:09:02 +0300

    Added animation logic for menu elements

---
+++ Showcase/Assets/Content/Source/UI/MenuTabs/MenuElementAnimation.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading;
+using Cysharp.Threading.Tasks;
+using DG.Tweening;
+using UnityEngine;
+
+namespace Source.UI
+{
+public class MenuElementAnimation : MonoBehaviour
+{
+    [SerializeField] CanvasGroup _cgItem;
+    
+    public Tween GetShowAnim(bool skipAnimation) =>
+        _cgItem.DOFade(1, ConstUIAnimation.GetAnimDuration(skipAnimation));
+
+    public UniTask GetHideAnim(bool skipAnimation, CancellationToken token) =>
+        _cgItem.DOFade(0, ConstUIAnimation.GetAnimDuration(skipAnimation)).ToUniTask(TweenCancelBehaviour.Complete, token);
+}
+}
