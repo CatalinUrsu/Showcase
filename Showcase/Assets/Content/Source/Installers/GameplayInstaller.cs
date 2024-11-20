a9c2c4a6b54bc7483ab71f4838534a9c20d2214a
Cat <cat.ursu30@gmail.com>
2024-11-20 16:15:23 +0300

    Added GameplayInstaller

---
+++ Showcase/Assets/Content/Source/Installers/GameplayInstaller.cs
@@ -0,0 +1,18 @@
+﻿using Zenject;
+using Source.MVP;
+using UnityEngine;
+using Source.Gameplay;
+
+namespace Source
+{
+public class GameplayInstaller : MonoInstaller
+{
+    [SerializeField] GameplayMediator _gameplayMediator;
+
+    public override void InstallBindings()
+    {
+        Container.Bind<GameProgressModel>().FromInstance(new GameProgressModel()).AsSingle();
+        Container.Bind<IGameplayMediator>().FromInstance(_gameplayMediator).AsSingle();
+    }
+}
+}
