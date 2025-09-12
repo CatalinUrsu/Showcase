c695f483d9b000d1672f2a4b9c932f49c68aa2ec
Cat <cat.ursu30@gmail.com>
2025-09-12 14:38:52 +0300

    Add additional comand to Helper's SaveSystemEditor's logic

---
+++ Showcase/Assets/Content/Source/Editor/SaveSystemCommandsAdditional.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using Helpers.Editor;
+using Source.Session;
+
+namespace Source.Editor
+{
+public abstract class SaveSystemCommandsAdditional : SaveSystemCommands
+{
+    [MenuItem("Tools/Helpers/SaveAll", false, 2)]
+    public static void Save() => SessionManager.Current.Save(ESaveFileType.All);
+}
+}
