8303317d18eb29b067f5ef6d5f04ae85920097d2
Cat <cat.ursu30@gmail.com>
2026-08-27 19:58:13 +0300

    Fix lose panel

--- Showcase/Assets/Content/Source/UI/Gameplay/GamePaneLoose.cs
+++ Showcase/Assets/Content/Source/UI/Gameplay/GamePaneLoose.cs
@@ -1,5 +1,4 @@
 ﻿using R3;
-using TMPro;
 using Zenject;
 using UnityEngine;
 using IdleNumbers;
@@ -14,14 +13,17 @@ public class GamePaneLoose : GamePanel
 
     [Space]
     [SerializeField] LocalizeStringEvent _txtCollectedCoins;
-    [SerializeField] TextMeshProUGUI _txtTotal;
+    [SerializeField] LocalizeStringEvent _txtTotal;
     
     [Space]
     [SerializeField] ButtonBase _buttonRestart;
     [SerializeField] ButtonBase _buttonHome;
 
-    [Inject] IGameRunModelController _gameRunModelController;
     readonly StringVariable _coinsStringVar = new();
+    readonly StringVariable _totalStringVar = new();
+    
+    [Inject] IGameRunModelController _gameRunModelController;
+    [Inject] IProgressModelController _progressModelController;
 
 #endregion
 
@@ -35,16 +37,31 @@ public class GamePaneLoose : GamePanel
         _buttonRestart.Init();
         
         _txtCollectedCoins.StringReference.Arguments = new[] { _coinsStringVar };
+        _txtTotal.StringReference.Arguments = new[] { _totalStringVar };
 
         _buttonHome.Btn.onClick.AddListener(() => _gameRunModelController.OnClickGoHome_raise());
         _buttonRestart.Btn.onClick.AddListener(() => _gameRunModelController.OnClickRestartRun_raise());
+        
         _gameRunModelController.IModel.CollectedCoinsRef
-                               .Skip(1)
                                .Subscribe(UpdateCollectedCoinsValue)
+                               .AddTo(this);
+        
+        _progressModelController.IModel.CoinsRef
+                               .Subscribe(UpdateTotalCoinsValue)
                                .AddTo(this);
     }
 
-    void UpdateCollectedCoinsValue(IdleNumber value) => _coinsStringVar.Value = value.AsString();
+    void UpdateCollectedCoinsValue(IdleNumber value)
+    {
+        _coinsStringVar.Value = value.AsString();
+        _txtCollectedCoins.RefreshString();
+    }
+
+    void UpdateTotalCoinsValue(IdleNumber value)
+    {
+        _totalStringVar.Value = value.AsString();
+        _txtTotal.RefreshString();
+    }
 
 #endregion
 }
